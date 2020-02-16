d57cabe25a771598a8c2c4fae470f350d20f3c72
sawanttej <61007735+sawanttej@users.noreply.github.com>
2020-02-16 20:51:41 +0530

    java sript project

---
+++ fun.js
@@ -0,0 +1,72 @@
+let queen = {
+    direction: "s",
+    position: "X , Y ",
+
+};
+
+function changedirection(N, S, E, W, NE, NW, SE, SW) {
+
+    var direction = "change direction";
+
+    switch (direction) {
+        case "direction N":
+            console.log("direction N");
+            break;
+
+        case "direction S":
+            console.log("direction S");
+            break;
+
+        case "direction E":
+            console.log("direction E");
+            break;
+
+        case "direction W":
+            console.log("direction W");
+            break;
+
+        case "direction NE":
+            console.log("direction NE");
+            break;
+
+        case "direction NW":
+            console.log("direction NW");
+            break;
+
+        case "direction SE":
+            console.log("direction SE");
+            break;
+
+        case "direction SW":
+            console.log("direction SW");
+            break;
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+function MoveForward(N, S, E, W, NE, NW, SE, SW) {
+
+    var move = N,
+        S, E, W, NE, NW, SE, SW;
+
+    for (i = 1; i < 8; i++);
+    if (y + 1 < 8)
+
+
+
+
+
+
+
+
+
+}
